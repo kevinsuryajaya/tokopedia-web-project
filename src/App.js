796b5a2aa2a35276465594aea3048f26dd241360
Kevin Suryajaya Soemarto <kevinsuryajaya08@gmail.com>
2020-11-17 19:12:04 +0700

    remove import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import * as React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import styled from "styled-components";
-import { Container } from "reactstrap";
 import "./App.css";
 
 import NavigationBar from "./components/navigation-bar";
 import PokemonList from "./pages/pokemon-list";
 import PokemonUser from "./pages/pokemon-user";
 
-const CustomContainer = styled(Container)`
+const CustomContainer = styled.div`
   padding-top: 5rem;
+  margin-bottom:3rem;
+  padding-left: 15px;
+  padding-right: 15px;
+  margin-left:auto;
+  margin-right:auto;
 `;
 
 export default function App() {
@@ -18,7 +22,7 @@ export default function App() {
       <NavigationBar />
       <CustomContainer>
         <Switch>
-          <Route path="/pokemon-detail" />
+          <Route path="/pokemon-detail/:id" />
           <Route path="/user" component={PokemonUser}/>
           <Route path="/" component={PokemonList} />
         </Switch>
