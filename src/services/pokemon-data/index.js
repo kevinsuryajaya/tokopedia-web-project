e0f62da1e2c9c9d06cf313203dded9752cc8157a
Kevin Suryajaya Soemarto <kevinsuryajaya08@gmail.com>
2020-11-17 19:21:17 +0700

    create service to get the API

---
+++ src/services/pokemon-data/index.js
@@ -0,0 +1,19 @@
+export async function getAllData(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      });
+  });
+}
+
+export async function getData(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      });
+  });
+}
