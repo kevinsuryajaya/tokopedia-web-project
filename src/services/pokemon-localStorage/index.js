b3be30fab6be8bf765e4384d3147ce46b4af23c5
Kevin Suryajaya Soemarto <kevinsuryajaya08@gmail.com>
2020-11-20 21:18:16 +0700

    get the storage

---
+++ src/services/pokemon-localStorage/index.js
@@ -0,0 +1,11 @@
+export function allStorage() {
+  var values = [],
+    keys = Object.keys(localStorage),
+    i = keys.length;
+
+  while (i--) {
+    values.push(JSON.parse(window.localStorage.getItem(keys[i])));
+  }
+
+  return values;
+}
